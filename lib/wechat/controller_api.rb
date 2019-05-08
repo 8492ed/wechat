68d0ec7d64a008a6e804421c0abf489ce0928e84
TheAnomaly <thekingsolomon6@gmail.com>
2019-05-08 13:51:59 +0800

    adding custom public account oauth2 function

--- lib/wechat/controller_api.rb
+++ lib/wechat/controller_api.rb
@@ -1,3 +1,5 @@
+require 'wechat/http_client'
+
 module Wechat
   module ControllerApi
     extend ActiveSupport::Concern
@@ -10,6 +12,42 @@ module Wechat
     def wechat(account = nil)
       # Make sure user can continue access wechat at instance level similar to class level
       self.class.wechat(account)
+    end
+
+    def custom_wechat_oauth2(scope = 'snsapi_base', page_url = nil, account = nil, &block)
+      puts "inside custom_wechat_oauth2".yellow
+      # ensure wechat initialization
+      self.class.corpid || self.class.appid || self.class.wechat
+
+      api = wechat(account)
+      if account
+        config = Wechat.config(account)
+        appid = config.corpid || config.appid
+        is_crop_account = !!config.corpid
+      else
+        appid = self.class.corpid || self.class.appid
+        is_crop_account = !!self.class.corpid
+      end
+
+      raise 'Can not get corpid or appid, so please configure it first to using wechat_oauth2' if appid.blank?
+
+      oauth2_params = {
+        appid: appid,
+        redirect_uri: page_url || generate_redirect_uri(account),
+        scope: scope,
+        response_type: 'code',
+        state: api.jsapi_ticket.oauth2_state
+      }
+
+      puts "inside wechat_public_oauth2".yellow
+      openid  = cookies.signed_or_encrypted[:we_openid]
+      unionid = cookies.signed_or_encrypted[:we_unionid]
+      we_token = cookies.signed_or_encrypted[:we_access_token]
+
+      puts "here 3".yellow
+      puts "oauth params #{oauth2_params}".yellow
+      puts "url #{generate_oauth2_url(oauth2_params)}".yellow
+      redirect_to generate_oauth2_url(oauth2_params)
     end
 
     def wechat_oauth2(scope = 'snsapi_base', page_url = nil, account = nil, &block)
@@ -40,7 +78,36 @@ module Wechat
       is_crop_account ? wechat_corp_oauth2(oauth2_params, account, &block) : wechat_public_oauth2(oauth2_params, account, &block)
     end
 
+    def wechat_user_info(account = nil, code)
+      puts "wechat_user_info #{account},#{code}".yellow
+      api = wechat(account)
+      if account
+        config = Wechat.config(account)
+        appid = config.corpid || config.appid
+        secret = config.secret
+      else
+        appid = self.class.corpid || self.class.appid
+        is_crop_account = !!self.class.corpid
+      end
+      raise 'Can not get corpid or appid, so please configure it first to using wechat_oauth2' if appid.blank?
+
+      results =  get_oauth2_access_token(code,appid,secret)
+      puts "results 1 #{results}".red
+      puts "access_token #{results["access_token"]}".red
+      puts "openid #{results["openid"]}".red
+      client = HttpClient.new('https://api.weixin.qq.com/sns/', 20, true)
+      results = client.get "userinfo?access_token=#{results["access_token"]}&openid=#{results["openid"]}&lang=zh_CN"
+      puts "results #{results}".red 
+      return results
+    end
+
     private
+
+    def get_oauth2_access_token(code,appid,secret)
+      puts "get_oauth2_access_token #{code},#{appid},#{secret} ".yellow
+      client = HttpClient.new('https://api.weixin.qq.com/sns/', 20, true)
+      client.get "oauth2/access_token?appid=#{appid}&secret=#{secret}&code=#{code}&grant_type=authorization_code"
+    end
 
     def wechat_public_oauth2(oauth2_params, account = nil)
       openid  = cookies.signed_or_encrypted[:we_openid]
